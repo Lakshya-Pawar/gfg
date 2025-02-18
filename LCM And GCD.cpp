b9c1e2f5e66a29e666b0fdeccf3c0623f43c18d3
Lakshya Pawar <156119162+Lakshya-Pawar@users.noreply.github.com>
2025-02-18 19:38:34 +0530

    Create LCM And GCD.cpp

---
+++ LCM And GCD.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> lcmAndGcd(int a, int b) {
+        // code here
+        vector<int> ans;
+        int maxn = max(a,b);
+        int i, j;
+        int gcd=0;
+        for(i=1; i<=maxn; i++){
+            if(a%i==0 && b%i==0){
+                gcd = i;
+            }
+        }
+        int lcm = (a*b)/gcd;
+        ans.push_back(lcm);
+        ans.push_back(gcd);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int A, B;
+
+        cin >> A >> B;
+
+        Solution ob;
+        vector<int> ans = ob.lcmAndGcd(A, B);
+        cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
